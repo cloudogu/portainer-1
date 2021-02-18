8242e5cc5d68b85b1e6ff7405f00e0bee566c7a4
Joshua Sprey <joshua.sprey@cloudogu.com>
2021-02-18 15:35:57 +0100

    [#6] Removed unecessary test case

--- api/exec/utils_test.go
+++ api/exec/utils_test.go
@@ -5,11 +5,6 @@ import (
 )
 
 func Test_isBinaryPresent(t *testing.T) {
-
-	if !IsBinaryPresent("docker") {
-		t.Error("expect docker binary to exist on the path")
-	}
-
 	if IsBinaryPresent("executable-with-this-name-should-not-exist") {
 		t.Error("expect binary with a random name to be missing on the path")
 	}
